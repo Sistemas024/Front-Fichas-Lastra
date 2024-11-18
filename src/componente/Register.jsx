2647a603feca7cff7c85fbdb2c4114bad8eb98cb
Juan Granja <4684593@gmail.com>
2024-11-18 16:28:06 -0500

    apunt

--- src/componente/Register.jsx
+++ src/componente/Register.jsx
@@ -12,7 +12,7 @@ const Registro = () => {
     const handleSubmit = async (event) => {
       event.preventDefault();
       
-      const response = await fetch('http://localhost:5000/auth/register', {
+      const response = await fetch('https://backend-fichas-lastra-8rzfkygai-sistemas-projects-de431368.vercel.app/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
